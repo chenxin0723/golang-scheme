919786aef7466ce6f389efdc7dad988fb886f927
chenxin0723 <870720136@qq.com>
2019-02-26 22:45:35 +0800

    add the Validate and Format

--- schema.go
+++ schema.go
@@ -7,16 +7,20 @@ import (
 	"reflect"
 )
 
+// type People struct {
+// 	Name string `field: "name" format: "" validator "" required`
+// }
+
 type SchemaValidator struct {
-	req             *http.Request
-	formatFuncMap   map[string]func(in interface{}) (out interface{}, err error)
-	validateFuncMap map[string]func(in interface{}) bool
+	req              *http.Request
+	formatFuncMap    map[string]func(in string) (out interface{}, err error)
+	validatorFuncMap map[string]func(in string) (bool, error)
 }
 
 type Config struct {
-	Request         *http.Request
-	FormatFuncMap   map[string]func(in interface{}) (out interface{}, err error)
-	ValidateFuncMap map[string]func(in interface{}) bool
+	Request          *http.Request
+	FormatFuncMap    map[string]func(in string) (out interface{}, err error)
+	ValidatorFuncMap map[string]func(in string) (bool, error)
 }
 
 func NewSchemaValidator(Config Config) (SchemaValidator, error) {
@@ -28,7 +32,7 @@ func NewSchemaValidator(Config Config) (SchemaValidator, error) {
 	}
 
 	schemaValidator.formatFuncMap = Config.FormatFuncMap
-	schemaValidator.validateFuncMap = Config.ValidateFuncMap
+	schemaValidator.validatorFuncMap = Config.ValidatorFuncMap
 	return schemaValidator, nil
 }
 
@@ -37,5 +41,55 @@ func (schemaValidator SchemaValidator) Encode(in interface{}) error {
 	if value.Kind() != reflect.Struct {
 		return errors.New(fmt.Sprintf("%s should be a struct kind", in))
 	}
+	inType := reflect.TypeOf(in)
 
+	for i := 0; i < inType.NumField(); i++ {
+		field := inType.Field(i)
+		name := field.Name
+		tag := field.Tag
+		var required bool
+		if fieldName := tag.Get("field"); fieldName != "" {
+			name = fieldName
+		}
+		if _, ok := tag.Lookup("required"); ok {
+			required = true
+		}
+
+		formStr := schemaValidator.req.FormValue(name)
+		var formValue interface{}
+		formValue = formStr
+
+		if formStr == "" && required {
+			return errors.New(fmt.Sprintf("%s is required", name))
+		}
+
+		if validatorName := tag.Get("validator"); validatorName != "" {
+			if passed, err := schemaValidator.Validate(validatorName, formStr); passed == false {
+				return err
+			}
+		}
+
+		if formatName := tag.Get("format"); formatName != "" {
+			if v, err := schemaValidator.Format(validatorName, formStr); err != nil {
+				return err
+			} else {
+				formValue = v
+			}
+		}
+
+	}
+}
+
+func (schemaValidator SchemaValidator) Validate(validatorName, formStr string) (bool, error) {
+	if validator, ok := schemaValidator.validatorFuncMap[validatorName]; ok {
+		return validator(formStr)
+	}
+	return false, errors.New(fmt.Sprintf("validator %s is nil", validatorName))
+}
+
+func (schemaValidator SchemaValidator) Format(formatName, formStr string) (out interface{}, err error) {
+	if formatName, ok := schemaValidator.formatFuncMap[formatName]; ok {
+		return formatName(formStr)
+	}
+	return nil, errors.New(fmt.Sprintf("format %s is nil", formatName))
 }
